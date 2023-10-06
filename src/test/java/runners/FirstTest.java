87aa636b02b23cb644c8df4fc81ae705fe0ea01c
Emre Demirtekin <edemirtekin11@gmail.com>
2023-10-06 19:22:15 +0300

    Initial commit

---
+++ src/test/java/runners/FirstTest.java
@@ -0,0 +1,18 @@
+package runners;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("JENKINS TE PROJEYİ ÇALIŞTIRMA");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.amazon.com");
+
+
+    }
+}
